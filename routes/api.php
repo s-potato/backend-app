133cd4dca02eff09b308bcb1401dda157420cd68
s-potato <sssssasas09@gmail.com>
2022-11-09 09:21:15 +0700

    change register path

--- routes/api.php
+++ routes/api.php
@@ -16,12 +16,14 @@ use App\Http\Controllers\Auth\AuthController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('register', [AuthController::class, 'register']);
+
 Route::prefix('user')->group(function() {
     Route::get('me', [AuthController::class, 'me']);
 });
 
 Route::prefix('auth')->group(function () {
-    Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
 
     Route::middleware(['auth:api'])->group(function () {
